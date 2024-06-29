37d27d2629a486644224c19a8743571cdaed9d75
Nouran-mamdouh <164439278+Nouran-mamdouh@users.noreply.github.com>
2024-06-29 14:35:32 +0300

    Update moodtracking.dart

--- lib/screens/moodtracking.dart
+++ lib/screens/moodtracking.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/camera_access.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Mood extends StatefulWidget {
   const Mood({super.key});
@@ -14,72 +15,62 @@ class _MoodState extends State<Mood> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 200,
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 200,
+                ),
+                child: Image.asset(
+                  'Images/iconoir_face-id (1).png',
+                  height: 200,
+                  width: 200,
+                  color: Colors.black,
+                ),
               ),
-              child: Image.asset(
-                'Images/iconoir_face-id (1).png',
-                height: 140,
-                width: 140,
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 60),
-              child: Text(
-                "“Tranquil” Would Like To Access The Camera",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 22,
-                    fontWeight: FontWeight.w700),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 60),
+                child: Text(
+                  "Face Detection",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w700),
+                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "This application is entirely based on MOOD TRACKING",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 22,
-                    fontWeight: FontWeight.w400),
+              const SizedBox(
+                height: 40,
               ),
-            ),
-            const SizedBox(
-              height: 60,
-            ),
-            InkWell(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (builder) => const Camera1()));
-              },
-              child: Container(
-                height: 50,
-                width: 280,
-                decoration: const BoxDecoration(
-                    color: Color(0XFFB799FF),
-                    borderRadius: BorderRadius.all(Radius.circular(12))),
-                child: const Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Text(
-                    "Get Started",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white),
+              InkWell(
+                onTap: () => launchUrl(
+                    Uri.parse('https://a0fc-197-35-250-161.ngrok-free.app/')),
+                child: Container(
+                  height: 50,
+                  width: 280,
+                  decoration: const BoxDecoration(
+                      color: Color(0XFFB799FF),
+                      borderRadius: BorderRadius.all(Radius.circular(12))),
+                  child: const Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Text(
+                      "Get Started",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
